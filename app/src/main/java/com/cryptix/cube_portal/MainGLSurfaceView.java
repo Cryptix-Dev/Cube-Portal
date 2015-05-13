102cc5ccc1c017c89dc7366e8630b0323e2e684b
Kyle Bartz <kyle@unleashurgeek.com>
2015-05-12 20:58:39 -0700

    Removed android.util.log import in GLSurfaceView

--- app/src/main/java/com/cryptix/cube_portal/MainGLSurfaceView.java
+++ app/src/main/java/com/cryptix/cube_portal/MainGLSurfaceView.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.SystemClock;
-import android.util.Log;
 
 public class MainGLSurfaceView extends GLSurfaceView {
 
